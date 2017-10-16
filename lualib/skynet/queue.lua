c71f5d173f7f49d212c43e6d2483a5d6aa9b1c83
jackiesun8 <jackieesun8@yeah.net>
2017-10-16 16:57:31 +0800

    up

--- lualib/skynet/queue.lua
+++ lualib/skynet/queue.lua
@@ -10,21 +10,21 @@ function skynet.queue()
 	local thread_queue = {}
 
 	return function(f, ...)
-		local thread = coroutine.running()
-		if current_thread and current_thread ~= thread then
-			table.insert(thread_queue, thread)
-			skynet.wait()
-			assert(ref == 0)	-- current_thread == thread
+		local thread = coroutine.running() -- 获取当前运行的协程
+		if current_thread and current_thread ~= thread then -- 如果是其他协程调用
+			table.insert(thread_queue, thread) -- 插入到队列里
+			skynet.wait()--阻塞其他协程
+			assert(ref == 0)	-- current_thread == thread --当该协程恢复时，断言引用必然为0
 		end
-		current_thread = thread
+		current_thread = thread -- 保存第一次调用的协程
 
-		ref = ref + 1
-		local ok, err = xpcall(f, traceback, ...)
-		ref = ref - 1
+		ref = ref + 1 --增加引用计数
+		local ok, err = xpcall(f, traceback, ...)--调用函数
+		ref = ref - 1 --调用结束，减少引用计数
 		if ref == 0 then
-			current_thread = table.remove(thread_queue,1)
+			current_thread = table.remove(thread_queue,1)--从队列里取出一个协程
 			if current_thread then
-				skynet.wakeup(current_thread)
+				skynet.wakeup(current_thread)--唤醒协程运行
 			end
 		end
 		assert(ok,err)
